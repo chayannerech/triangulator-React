6d281935fdc7a52696f8a7a19cf426995a1ee0e1
chay <rechchayanne@gmail.com>
2024-02-15 19:59:07 -0300

    Triangulipse

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import TriangleCalculator from "./TriangleCalculator"
+import { Toaster } from "./components/ui/toaster"
+
+function App() {
+  return (
+    <main className="max-w-6xl mx-auto my-12">
+      <h1 className="mb-4">Incrível e Maravilhosa Calculadora de Triangulos</h1>
+      <TriangleCalculator />
+      <Toaster />
+    </main>
+  )
+}
+
+export default App
